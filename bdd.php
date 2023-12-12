ae4061dba2bfb516f7e68e68cb9caa96b367a209
TorayLaTortue <damien.pasquer3636@gmail.com>
2023-12-12 14:12:41 +0100

    esperon qu'il n'y a plus de probleme

--- bdd.php
+++ bdd.php
@@ -7,37 +7,46 @@
         $host = "localhost";
         $dbname = "Matching";
         $user = "postgres";
-        $password = "Vemer835";
+        $password = "Vemer";
         
 
         // Connexion à la base de données
         try{
-        $bdd = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+        $bdd = new PDO("pgsql:host=$host;port=5432;dbname=$dbname", $user, $password);
         $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);}
         catch(PDOException $e){// Vérifier la connexion
         die("Échec de la connexion à la base de données : " . $e->getMessage());}
 
-       // Requête SQL pour récupérer des données d'une table (remplacez "votre_table" par le nom de votre table)
-        $sql = "SELECT * FROM Cosplayeur";
-        $result = $conn->query($sql);
+// Requête SQL pour récupérer des données de la table Convention
+$sql = 'SELECT * FROM "Convention" c';
 
-        // Vérifier si la requête a réussi
-        if ($result) {
+// Exécution de la requête SQL
+try {
+    $result = $bdd->query($sql);
+
+    // Vérifier si la requête a réussi
+    if ($result) {
         // Parcourir les résultats de la requête
-        while ($row = $result->fetch_assoc()) {
-        // Afficher les données (vous pouvez ajuster ceci en fonction de votre table)
-        echo "idCosplayeur : " . $row["idCosplayeur"] . "<br>";
-        //echo "Nom : " . $row["nom"] . "<br>";
-        //echo "Description : " . $row["description"] . "<br>";
-        // Ajoutez d'autres champs selon votre table
-        echo "<br>";}
-    // Libérer le résultat
-    $result->free();
-    } else {
-    // Afficher une erreur si la requête a échoué
-    echo "Erreur lors de l'exécution de la requête : " . $conn->error;
-    }
+        while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            // Afficher les données (vous pouvez ajuster ceci en fonction de votre table)
+            echo "NomConvention : " . $row["NomConvention"] . "<br>";
+            echo "NomPays : " . $row["NomPays"] . "<br>";
+            echo "NomRegion : " . $row["NomRegion"] . "<br>";
+            echo "idConvention : " . $row["idConvention"] . "<br>";
+            echo "<br>";
+        }
 
-    // Fermer la connexion à la base de données
-    $conn->close();
+        // Libérer le résultat
+        $result->closeCursor();
+    } else {
+        // Afficher une erreur si la requête a échoué
+        echo "Erreur lors de l'exécution de la requête : " . print_r($bdd->errorInfo(), true);
+    }
+} catch (PDOException $e) {
+    // Gérer les exceptions PDO
+    echo "Erreur PDO : " . $e->getMessage();
+}
+
+// Fermer la connexion à la base de données
+$bdd = null;
 ?>
